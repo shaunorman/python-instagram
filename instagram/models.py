2cc6fabb032209160cc1e72c60eefae5859f3f74
Shaun Norman <shaun@hatchd.com.au>
2014-02-17 11:01:49 +0800

    spaces for indentation

--- instagram/models.py
+++ instagram/models.py
@@ -41,7 +41,7 @@ class Media(ApiModel):
     def object_from_dictionary(cls, entry):
         new_media = Media(id=entry['id'])
 
-	new_media.type = entry['type']
+        new_media.type = entry['type']
 
         new_media.user = User.object_from_dictionary(entry['user'])
         new_media.images = {}
